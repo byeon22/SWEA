f7922a937de201cc6d5596fc942626e6cf250542
Minjeong <117675757+byeon22@users.noreply.github.com>
2023-11-17 02:35:27 +0900

    [D2] Title: 쉬운 거스름돈, Time: 109 ms, Memory: 19,504 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,34 +1,55 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-        int t = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int money = Integer.parseInt(st.nextToken());
+			int[] coins = new int[8];
 
-        for (int i = 1; i <= t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            change(i, n);
-        }
+			// 고려해야 할 것: 5000원 1000원 500원 100원 50원 10원
+			int cnt = 0;
+			while (money > 0) { // 1999990 1000 
+				cnt++;
+				if (cnt == 2) { // 10원 50원
+					if (money % 10 >= 5)
+						coins[6]++;
+					coins[7] += (money % 10) % 5;
+				} else if (cnt == 3) {
+					if (money % 10 >= 5)
+						coins[4]++;
+					coins[5] += (money % 10) % 5;
+				} else if (cnt == 4) {
+					if (money % 10 >= 5)
+						coins[2]++;
+					coins[3] += (money % 10) % 5;
+				} else if (cnt == 5) {
+					if (money % 10 >= 5)
+						coins[0]++;
+					coins[1] += (money % 10) % 5;
+				} else if (cnt >= 6) {
+					coins[0] += money * 10 / 5;
+					break;
+				}
 
-        br.close();
-    }
-
-    private static void change(int testCase, int n) {
-        int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-        int[] count = new int[8];
-
-        for (int i = 0; i < money.length; i++) {
-            count[i] = n / money[i];
-            n %= money[i];
-        }
-
-        System.out.print("#" + testCase + "\n");
-        for (int i = 0; i < money.length; i++) {
-            System.out.print(count[i] + " ");
-        }
-        System.out.println();
-    }
+				money /= 10;
+			}
+			sb.append("#").append(i).append("\n");
+			for (int j = 0; j < 8; j++) {
+				sb.append(coins[j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
