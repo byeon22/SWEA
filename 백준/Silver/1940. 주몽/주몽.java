7bded302c46de46316cf557fafe16b82e32ff381
Minjeong <117675757+byeon22@users.noreply.github.com>
2024-04-11 14:38:56 +0900

    [Silver IV] Title: 주몽, Time: 348 ms, Memory: 24932 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] list = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(list);
+
+        int start = 0;
+        int end = start + 1;
+        int sum = list[start];
+        int count = 0;
+
+        while (start < N-1 && end < N) {
+            sum += list[end];
+            if (sum < M || end <= start) {
+                sum -= list[end];
+                end++;
+                if (end >= N) {
+                    start++;
+                    sum = list[start];
+                    end = start+1;
+                }
+            } else {
+                if (sum == M) count++;
+                start++;
+                sum = list[start];
+                end = start+1;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
