463aeb56d10c817f045c13df0e0ef1b1068c4a73
Minjeong <117675757+byeon22@users.noreply.github.com>
2024-05-10 23:47:12 +0900

    [D2] Title: 달팽이 숫자, Time: 104 ms, Memory: 22,712 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,63 +1,53 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
-class Solution {
-	// 달팽이 어려워.. 
-	// 블로그 참고: https://velog.io/@dudrkdl777/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98SWEA-1954.-%EB%8B%AC%ED%8C%BD%EC%9D%B4-%EC%88%AB%EC%9E%90D2-Java
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// PrintWriter pw = new PrintWriter("C:\\Users\\samsung\\Downloads\\result.txt");
-		StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int N;
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int d;
+        int row, col;
+        int num;
+        int[][] snail;
 
-		int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            snail = new int[N][N];
 
-		for (int i = 1; i <= T; i++) {
-			sb.append("#").append(i).append(" ").append("\n");
-			int N = Integer.parseInt(br.readLine());
-			
-			int[][] box = new int[N][N];
-			boolean[][] mark = new boolean[N][N];
-			//        -1,0
-			// 0,-1 <- 0,0 -> 0,1
-			//         1,0 
-			int[] dx = new int[]{0, 1, 0, -1}; // 우, 하, 좌, 상
-			int[] dy = new int[]{1, 0, -1, 0};
-			
-			int dir = 0;
-			int x = 0; // box의 좌표
-			int y = 0;
-			for (int j = 1; j <= N*N; j++) {
-				box[x][y] = j;
-				mark[x][y] = true;
-				
-				int nextx = x + dx[dir];
-				int nexty = y + dy[dir];
-				
-				if (nextx >= N || nexty >= N || nextx < 0 || nexty < 0 || mark[nextx][nexty]) {
-					dir = (dir + 1) % 4;
-				}
-				
-				x += dx[dir];
-				y += dy[dir];
-			}
-			
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < N; k++) {
-					sb.append(box[j][k]).append(" ");
-				}
-				sb.append("\n");
-			}
-		}
+            d = 0;
+            row = 0;
+            col = 0;
+            num= 1;
 
-		System.out.println(sb);
-		// pw.println(sb);
-		// pw.close();
-	}
+            while (num <= N*N) {
+                snail[row][col] = num;
+                num++;
+
+                if (row + dy[d] >= N || col + dx[d] >= N || row + dy[d] < 0 || col + dx[d] < 0 || snail[row + dy[d]][col + dx[d]] != 0) {
+                    d = (d+1)%4;
+                }
+
+                row += dy[d];
+                col += dx[d];
+            }
+
+            sb.append("#").append(tc+1).append("\n");
+
+            for (row = 0; row < N; row++) {
+                for (col = 0; col < N; col++) {
+                    sb.append(snail[row][col]).append(" ");
+                }
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
