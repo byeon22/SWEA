f90e8939300938395e554c823922890e753b1fb5
byeon22 <minjeongs0202@gmail.com>
2024-08-10 00:51:39 +0900

    6. 중복문자제거

--- 인프런/ch01/prob.01-06/src/Sol1.java
+++ 인프런/ch01/prob.01-06/src/Sol1.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Sol1 {
+// indexOf 사용
+
+import java.util.Scanner;
+
+public class Sol1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            // 처음 등장하는 인덱스가 어디인지 체크
+            // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            if (str.indexOf(str.charAt(i)) == i) // i는 현재 문자의 위치, 현재 위치랑 처음 나오는 위치랑 동일하면 중복X
+                answer += str.charAt(i);
+        }
+
+        return answer;
+    }
 }
