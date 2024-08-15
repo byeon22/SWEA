f388908117742023c362ad4122419833f15d81f3
byeon22 <minjeongs0202@gmail.com>
2024-08-15 23:58:44 +0900

    1. 큰 수 출력하기

---
+++ 인프런/ch02/prob.02-01/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int pre = 0;
+
+        for (int x : array) {
+            if (pre < x) sb.append(x).append(" ");
+            pre = x;
+        }
+
+        System.out.println(sb);
+    }
+}
