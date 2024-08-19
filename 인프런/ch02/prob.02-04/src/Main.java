39693105cd01df07d1566e36d1479ab40f53a665
byeon22 <minjeongs0202@gmail.com>
2024-08-19 10:35:44 +0900

    4. 피보나치 수열

---
+++ 인프런/ch02/prob.02-04/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int sum = 1;
+        arr[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            arr[i] = sum;
+            sum = arr[i-1] + arr[i];
+        }
+
+        for (int x : arr) {
+            sb.append(x).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
